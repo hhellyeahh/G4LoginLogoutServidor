1536ac10f7452eb683e835ac2190cac21c4451ad
2dam <2dam@HZ375749.TartangaLH.eus>
2022-10-19 13:19:13 +0200

    First

---
+++ src/g4loginlogoutservidor/G4LoginLogoutServidor.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package g4loginlogoutservidor;
+
+/**
+ *
+ * @author 2dam
+ */
+public class G4LoginLogoutServidor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
